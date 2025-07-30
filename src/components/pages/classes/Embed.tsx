411d8874496d030ae96338cbaaf706b879a2bb16
benjapadilla1 <benja.padilla@outlook.com.ar>
2025-07-30 12:20:26 -0300

    test: testing payhip intergration reversing changes

--- src/components/pages/classes/Embed.tsx
+++ src/components/pages/classes/Embed.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 declare global {
   interface Window {
@@ -15,40 +15,40 @@ interface PayhipEmbedProps {
 }
 
 export const PayhipEmbedClasses = ({ dataKey }: PayhipEmbedProps) => {
-  const containerRef = useRef<HTMLDivElement>(null);
+  // const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const existingScript = document.querySelector(
-      'script[src="https://payhip.com/embed-page.js?v=24u68984"]'
-    );
+  // useEffect(() => {
+  //   const existingScript = document.querySelector(
+  //     'script[src="https://payhip.com/embed-page.js?v=24u68984"]'
+  //   );
 
-    if (!existingScript) {
-      const script = document.createElement('script');
-      script.src = 'https://payhip.com/embed-page.js?v=24u68984';
-      script.type = 'text/javascript';
-      script.async = true;
-      script.onload = () => {
-        if (window.PayhipEmbed) {
-          window.PayhipEmbed.loadEmbeds();
-        }
-      };
-      document.body.appendChild(script);
-    } else {
-      if (window.PayhipEmbed) {
-        window.PayhipEmbed.loadEmbeds();
-      } else {
-        existingScript.addEventListener('load', () => {
-          if (window.PayhipEmbed) {
-            window.PayhipEmbed.loadEmbeds();
-          }
-        });
-      }
-    }
-  }, []);
+  //   if (!existingScript) {
+  //     const script = document.createElement('script');
+  //     script.src = 'https://payhip.com/embed-page.js?v=24u68984';
+  //     script.type = 'text/javascript';
+  //     script.async = true;
+  //     script.onload = () => {
+  //       if (window.PayhipEmbed) {
+  //         window.PayhipEmbed.loadEmbeds();
+  //       }
+  //     };
+  //     document.body.appendChild(script);
+  //   } else {
+  //     if (window.PayhipEmbed) {
+  //       window.PayhipEmbed.loadEmbeds();
+  //     } else {
+  //       existingScript.addEventListener('load', () => {
+  //         if (window.PayhipEmbed) {
+  //           window.PayhipEmbed.loadEmbeds();
+  //         }
+  //       });
+  //     }
+  //   }
+  // }, []);
 
   return (
-    <div ref={containerRef} className="payhip-embed-page" data-key={dataKey}>
-      <p>Cargando clases 1 a 1 ...</p>
-    </div>
+    // <div ref={containerRef} className="payhip-embed-page" data-key={dataKey}>
+    <p>Cargando clases 1 a 1 ...</p>
+    // </div>
   );
 };
