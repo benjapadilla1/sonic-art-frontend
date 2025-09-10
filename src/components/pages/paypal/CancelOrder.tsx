2721d8d09d444a326c896e26f95c89f207640dc6
benjapadilla1 <benja.padilla@outlook.com.ar>
2025-09-10 14:41:07 -0300

    fix: fixing use search params

---
+++ src/components/pages/paypal/CancelOrder.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import axios from 'axios';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+export default function CancelOrder() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+
+  useEffect(() => {
+    const cancelOrder = async () => {
+      if (!token) return;
+
+      try {
+        const res = await axios.delete(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/paypal/cancel-order?token=${token}`
+        );
+
+        if (res.data) {
+          toast.info('Pago cancelado');
+        } else {
+          toast.error('Error al cancelar la orden');
+        }
+      } catch (err) {
+        console.error(err);
+        toast.error('Error de conexión con el servidor');
+      } finally {
+        router.push('/');
+      }
+    };
+
+    cancelOrder();
+  }, [router, token]);
+
+  return <p>Pago cancelado.</p>;
+}
